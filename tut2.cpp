9362fee211a9e5edf066a0a2ff4bd54004f8f5b6
Norvin30 <108176736+Norvin30@users.noreply.github.com>
2022-06-26 17:16:15 +0530

    Add files via upload

---
+++ tut2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+//This code was created by Norvin
+/*this 
+is
+a
+multipliesline
+comment */
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
+
+            
