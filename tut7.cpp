2731276c91b8c3b4eab69a3a3bbc21b994397fd6
Norvin30 <108176736+Norvin30@users.noreply.github.com>
2022-06-28 17:07:58 +0530

    Add files via upload

---
+++ tut7.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int glo=10;
+void sum(){
+    int a;
+}
+int main(){
+    int glo=79;
+    glo=80;
+    //int w=19;
+    //int s=20;
+    int w=19, s=20;
+    float pi=3.15;
+    cout <<glo;
+    cout<<"\nthis is tutorial 6.\n here w is "<<w<<".\n here s is "<<s;
+    cout<<"the value of pi is:"<<pi;
+     return 0;
+}
