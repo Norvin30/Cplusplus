32bdaaa5acac82ace9103741c45751e97735259c
Norvin30 <108176736+Norvin30@users.noreply.github.com>
2022-07-04 13:21:40 +0530

    Add files via upload

---
+++ tut11.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 34;
+    char c= 'c';
+    cout<<"The value of a was: "<<a;
+    a = 45;
+    cout<<"the value of a is: "<<a;
+    return 0;
+}
