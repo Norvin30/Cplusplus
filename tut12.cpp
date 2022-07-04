dba97523594ee37d15859d49dd1e91a7d08938f1
Norvin30 <108176736+Norvin30@users.noreply.github.com>
2022-07-04 13:22:26 +0530

    Add files via upload

---
+++ tut12.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+    // int a = 34;
+    // cout<<"the value of a was:"<<a;
+    // a = 45;
+    // cout<<"the value of a is:"<<a;
+    // constants in C++
+    // const int a = 3;
+    // cout<<"the value of a was:"<<a<<endl;
+    // a = 45; // you will get an error because a is a constant
+    // cout<<"the value of a is:"<<a<<endl;
+    int a =3, b=78, c=1233;
+    cout<<"The value of a is:"<<a<<endl;
+    cout<<"the value of a is:"<<a<<endl;
+    cout<<"the value of a is:"<<a<<endl;
+    cout<<"the value of b is:"<<b<<endl;
+    cout<<"the value of c is:"<<c<<endl;
+}
