6830aab250042ac8efca63829226abc5db6f533e
Norvin30 <108176736+Norvin30@users.noreply.github.com>
2022-07-04 13:20:45 +0530

    Add files via upload

---
+++ tut10.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=3;
+    int b=4;
+    cout<<"hello Norvin how are you.here the value of a="<<a<<". the value of b is"<<b;
+    return 0;
+}
